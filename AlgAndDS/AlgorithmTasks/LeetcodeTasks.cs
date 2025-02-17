d1313e2f5a25b79261792e27fa4abef4ac993004
Cladkoewka <codewarriorms@gmail.com>
2025-02-17 07:09:40 +0300

    leetcode 66

--- AlgAndDS/AlgorithmTasks/LeetcodeTasks.cs
+++ AlgAndDS/AlgorithmTasks/LeetcodeTasks.cs
@@ -304,4 +304,33 @@ public static class LeetcodeTasks
 
         return oldToNew[head];
     }
+    
+    /// <summary>
+    /// 66. Plus One
+    /// https://leetcode.com/problems/plus-one/description/?envType=problem-list-v2&envId=array
+    /// </summary>
+    /// <param name="digits"></param>
+    /// <returns></returns>
+    public static int[] PlusOne(int[] digits)
+    {
+        int n = digits.Length;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+        }
+
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+
+        return newDigits;
+    }
 }
