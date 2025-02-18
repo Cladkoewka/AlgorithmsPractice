92f50c2e4254cf9a64df25646d59c0add5117dbe
Cladkoewka <codewarriorms@gmail.com>
2025-02-18 07:53:08 +0300

    leetcode 36

--- AlgAndDS/obj/Debug/net8.0/AlgAndDS.AssemblyInfo.cs
+++ AlgAndDS/obj/Debug/net8.0/AlgAndDS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgAndDS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1002a3854b8b3736b4067addb79bccfc1bcafd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1313e2f5a25b79261792e27fa4abef4ac993004")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgAndDS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgAndDS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
