f6e82a6cbf84d0077f4dde7b5a9fa04d4d585062
Cladkoewka <codewarriorms@gmail.com>
2025-02-23 15:56:37 +0300

    selection sort realization

--- AlgAndDS/Algorithms/SortingAlgorithms.cs
+++ AlgAndDS/Algorithms/SortingAlgorithms.cs
@@ -23,4 +23,23 @@ public static class SortingAlgorithms
                 break;
         }
     }
+
+    public static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            if (minIndex != i)
+                (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+        }
+    }
 }
