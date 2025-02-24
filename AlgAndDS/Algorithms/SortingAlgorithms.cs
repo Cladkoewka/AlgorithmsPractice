2410d6e466e73f61ec1895a15155a78c4ba39bc1
Cladkoewka <codewarriorms@gmail.com>
2025-02-24 18:47:02 +0300

    Merge sort realization

--- AlgAndDS/Algorithms/SortingAlgorithms.cs
+++ AlgAndDS/Algorithms/SortingAlgorithms.cs
@@ -61,4 +61,47 @@ public static class SortingAlgorithms
             arr[j + 1] = key;
         }
     }
+
+    public static void MergeSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int mid = (right + left) / 2;
+            
+            MergeSort(arr, left, mid);
+            MergeSort(arr, mid + 1, right);
+
+            Merge(arr, left, mid, right);
+        }
+    }
+
+    public static void Merge(int[] arr, int left, int mid, int right)
+    {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) 
+            leftArray[i] = arr[left + i];
+        for (int i = 0; i < n2; i++)
+            rightArray[i] = arr[mid + i + 1];
+
+        int i1 = 0, i2 = 0, k = left;
+
+        while (i1 < n1 && i2 < n2)
+        {
+            if (leftArray[i1] <= rightArray[i2])
+                arr[k++] = leftArray[i1++];
+            else
+                arr[k++] = rightArray[i2++];
+        }
+
+        while (i1 < n1) 
+            arr[k++] = leftArray[i1++];
+
+        while (i2 < n2) 
+            arr[k++] = rightArray[i2++];
+    }
 }
--- AlgAndDS/obj/Debug/net8.0/AlgAndDS.AssemblyInfo.cs
+++ AlgAndDS/obj/Debug/net8.0/AlgAndDS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgAndDS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e82a6cbf84d0077f4dde7b5a9fa04d4d585062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ba8f9b404d253285a479b5e163ab94f84cab22")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgAndDS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgAndDS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
